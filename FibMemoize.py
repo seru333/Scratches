e2b857e0cc57052b206eaa1aba883e2771a0b752
Eric <seru333@gmail.com>
2019-01-15 21:12:30 -0700

    Playing with memoization in python using the fibonacci sequence

---
+++ FibMemoize.py
@@ -0,0 +1,26 @@
+class Memoize:
+    def __init__(self, entry):
+        self.entry = entry
+        self.memo = {}
+    def __call__(self, *args):
+        if not args in self.memo:
+            self.memo[args] = self.entry(*args)
+        return self.memo[args]
+@Memoize
+def fib(n):
+    if type(n) != int:
+        return print("please enter and integer")
+    if n < 1:
+        return print("not a positive integer")
+    if n == 1 or n == 2:
+        answer = 1
+    else:
+        answer = n * fib(n - 1)
+    return answer
+
+if __name__ == '__main__':
+    # assert fib(7) == 13
+    for i in range(1, 1000):
+        print(i, ":", fib(i))
+    fib(-1)
+    fib("one")
